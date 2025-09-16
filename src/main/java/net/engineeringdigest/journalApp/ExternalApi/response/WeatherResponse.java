c9d156a0f0beeef2affea8ee2351383ad93033d3
sshende-java <80737493+sshende-java@users.noreply.github.com>
2025-09-16 13:16:23 +0530

    Bug Fix

--- src/main/java/net/engineeringdigest/journalApp/ExternalApi/response/WeatherResponse.java
+++ src/main/java/net/engineeringdigest/journalApp/ExternalApi/response/WeatherResponse.java
@@ -1,20 +1,22 @@
 package net.engineeringdigest.journalApp.ExternalApi.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 
 @Data  //getter setter RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class WeatherResponse {
 
 
     private Current current;
 
     @Data  //getter setter RequiredArgsConstructor
-    public class Current {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Current {       //made this class as static so that Jackson can access it without instance
 
         public int temperature;
 
